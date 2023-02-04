dacdb00f152dcae573d9c06f67b4fe3a9c990db0
WillowMT <waiyanmt.20@icloud.com>
2023-02-04 22:45:35 +0800

    1st commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (
+        <>
+            <div className="w-96 h-40 rounded-lg bg-red-300 grid place-items-center">
+                Hello
+            </div>
+        </>
+    );
+}
